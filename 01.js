420f4a70cfa6efac11469b09b5826c6b0f5e1982
Rodrigo Ramirez <rodrigo8rb@htomail.com>
2020-04-16 15:09:41 -0500

    testing commit

---
+++ 01.js
@@ -0,0 +1,10 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Bienvenido a InterLab',
+        users: [
+            {name: 'Company', quantity: 0},
+            {name: 'Student', quantity: 0}
+        ]
+    }
+})
