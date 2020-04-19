bd029dc07dc8887fdebba8e165af7b9ceee1096b
Rodrigo Ramirez <rodrigo8rb@htomail.com>
2020-04-18 20:31:14 -0500

    Added assets and texts

--- 01.js
+++ 01.js
@@ -2,9 +2,11 @@ const app = new Vue({
     el: '#app',
     data: {
         body_bg: 'bg-dark',
-        title: 'INTERLAB',
+        title: 'InterLab',
         background: 'bg-warning',
-        color: true
+        color: true,
+        subtitle1: 'Obtén ofertas de pasantías relevantes',
+        text1: 'Consigue pasantías de todo el mundo, que se ajusten a tus necesidades, con solo responder unas preguntas simples.'
     },
     methods: {
 
